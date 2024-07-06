30cf72a7e5d2cd6f6a23b5a43dd0679a2c15d26e
gramatchi <nik98.champion@gmail.com>
2024-07-06 17:17:53 +0200

    Do task-5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const spanRef = document.querySelector(".color");
+const bodyRef = document.querySelector("body");
+const btnRef = document.querySelector(".change-color");
+
+btnRef.addEventListener("click", (e) => {
+  e.preventDefault();
+  const color = getRandomHexColor();
+  spanRef.textContent = `${color}`;
+  bodyRef.style.backgroundColor = color;
+});
