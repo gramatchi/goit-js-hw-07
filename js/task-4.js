45619c164b518c257bfda89ce5589ae44e96d02c
gramatchi <nik98.champion@gmail.com>
2024-07-06 17:05:07 +0200

    Do task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,18 @@
+const formRef = document.querySelector(".login-form");
+const emailInputRef = formRef.elements["email"];
+const passwordInputRef = formRef.elements["password"];
 
+formRef.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const userInfo = {};
+
+  if (!emailInputRef.value.trim() || !passwordInputRef.value.trim()) {
+    alert("All form fields must be filled in");
+  } else {
+    userInfo.email = emailInputRef.value.trim();
+    userInfo.password = passwordInputRef.value.trim();
+    console.log(userInfo);
+    formRef.reset();
+  }
+});
